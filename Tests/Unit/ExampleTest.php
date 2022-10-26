68f085e7ccdbd19a529c3f714eb0a0035105d24f
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-10-26 09:23:52 +0300

    initial commit

---
+++ Tests/Unit/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertTrue;
+
+class WindowTest extends TestCase
+{
+    public function testWindowFactoryCreate(): void
+    {
+        assertTrue(true);
+    }
+}
